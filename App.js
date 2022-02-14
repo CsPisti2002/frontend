cd7a1c3dc941d5178a3e0db97909b76c538c952d
CsPisti2002 <cseke.pisti2002@gmail.com>
2022-02-14 12:55:38 +0100

    proba

--- App.js
+++ App.js
@@ -1,21 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View,Text, StyleSheet } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Komment from './Komment';
+import Forum from './Forum';
+
+
+
+
+function komment_lap({ navigation }) {
+
+
+  return (
+
+<View style={{flex: 1, paddingTop:20,backgroundColor:' '}}>
+<Komment/>
+
+<Text style={{textAlign:'left'}}>
+       Ha elszeretnéd olvasni mások tapasztalatait, véleményeit vagy problémáit akkor kattints ide:
+      </Text>
+      <Button  
+      style={styles.gomb}
+      onPress={() => navigation.navigate('Forum')}
+        title="Kommentek megnézése"
+/>
+    </View>
+    
+  );
+}
+
+function forum_lap({ navigation }) {
+  return (
+
+
+
+<View style={{ flex: 1, alignItems:'center', justifyContent: 'center',backgroundColor:'#ff9966' }}>
+      <Text>
+        -.-
+      </Text>
+      
+      <Button
+        onPress={() => navigation.navigate('Komment')}
+        title="Komment írása"
+      />
+
+<Forum/>
+    </View>
+
+    
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+
+const ipcim=" 172.16.0.193";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+
+      <Drawer.Screen name="Komment" component={komment_lap}  
+options={{
+  title: 'Komment',
+  headerStyle: {
+    backgroundColor: '#2a0080',/*'#262729'*/
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+
+
+  
+}}/>
+
+
+<Drawer.Screen name="Forum" component={forum_lap}  
+options={{
+  title: 'Forum',
+  headerStyle: {
+    backgroundColor: '#2a0080',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+
+
+  
+}}/>
+
+        
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+  gomb:{
+          height:45,
+          backgroundColor:'#635320',
+          width:'45%',
+          alignSelf:'center',
+          borderRadius:10
   },
 });
